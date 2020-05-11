44ce4a3a4366310537b0196eb64f2425b28f152b
2016-04-07592 <josephkwagilwa@gmail.com>
2020-05-11 19:16:52 +0000

    after changing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var path = require('path');
-const cors = require('cors');
+var cors = require('cors');
 const app = express();
 
 //app.use(express.urlencoded())
